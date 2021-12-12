13b2ada44733a7e2f9d36241b8ae82bc7804ac2b
isminik <92885814+isminik@users.noreply.github.com>
2021-12-12 22:24:03 +0200

    Add files via upload

---
+++ Nereids eclipse/src/gr/aueb/dmst/nereids/graphicalInterface/MainOfWinDefeatFrames.java
@@ -0,0 +1,10 @@
+package gr.aueb.dmst.nereids.graphicalInterface;
+
+public class MainOfWinDefeatFrames {
+
+	public static void main(String[] args) {
+		//WinFrame wf1 = new WinFrame();
+		DefeatFrame df1 = new DefeatFrame();
+	}
+
+}
