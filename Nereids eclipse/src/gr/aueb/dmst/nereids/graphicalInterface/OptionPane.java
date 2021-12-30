6c5cc8267e28c72c73714058a4904533f4a96047
Paraskevi Argyropoulou <t8200014@aueb.gr>
2021-12-30 03:04:26 +0200

    fix option pane

--- Nereids eclipse/src/gr/aueb/dmst/nereids/graphicalInterface/OptionPane.java
+++ Nereids eclipse/src/gr/aueb/dmst/nereids/graphicalInterface/OptionPane.java
@@ -47,45 +47,42 @@ public class OptionPane {
 		} else if (dif.getName() == "disorientation") {
 			icon = new ImageIcon("Images/disorientation.png");
 		}
-		
-		// resize it to 180 x 150
-		icon = resize(icon, 180, 150);
+		// resize it to 200 x 180
+		icon = resize(icon, 200, 180);
 		JLabel label = new JLabel(icon);
 		JPanel panel = new JPanel(new GridBagLayout());
 		panel.add(label);
 		JPanel textPanel = new JPanel(new GridLayout(3, 1));
-		
+
 		textPanel.setFont(new Font("Tahoma", Font.BOLD, 19));
 		textPanel.add(new JLabel("LEVEL " + level + " "));
 		textPanel.add(new JLabel("You chose: " + dif.getName() + " "));
-		textPanel.add(new JLabel( "SCORE: " + Status.getScore() + " "));
-		
-					
-		
+		textPanel.add(new JLabel("SCORE: " + Status.getScore() + " "));
+
 		JPanel panel2 = new JPanel(new BorderLayout());
 		panel2.add(textPanel);
 		panel2.add(panel, BorderLayout.EAST);
 		JOptionPane.showMessageDialog(null, panel2, "Face Difficulty", JOptionPane.DEFAULT_OPTION);
 
-
 	}
+
 	/**
-	 * returns the given image with the specific ordered size 
-	 * taken from: 
+	 * returns the given image with the specific ordered size taken from:
 	 * http://www.java2s.com/example/java-utility-method/imageicon-resize/resize-imageicon-image-int-width-int-height-5ce1d.html
+	 * 
 	 * @param image
 	 * @param width
 	 * @param height
 	 * @return
 	 */
 	public static ImageIcon resize(ImageIcon image, int width, int height) {
-        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
-        Graphics2D g2d = (Graphics2D) bi.createGraphics();
-        g2d.addRenderingHints(//  ww  w  . jav  a2 s. c o m
-                new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
-        g2d.drawImage(image.getImage(), 0, 0, width, height, null);
-        g2d.dispose();
-        return new ImageIcon(bi);
-    }
+		BufferedImage bi = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
+		Graphics2D g2d = (Graphics2D) bi.createGraphics();
+		g2d.addRenderingHints(// ww w . jav a2 s. c o m
+				new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+		g2d.drawImage(image.getImage(), 0, 0, width, height, null);
+		g2d.dispose();
+		return new ImageIcon(bi);
+	}
 
 }
